282439dd189fc49a47a7cb29cd0b7b7b2c9af551
LopsemPyier <LopsemPyier@gmail.com>
2021-04-27 14:34:48 +0200

    HACK v0.1.3 Add ace editor
    FEAT:
     - Ace editor component for latex

--- src/app/editor/ui/editor/editor.component.ts
+++ src/app/editor/ui/editor/editor.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as ace from 'ace-builds';
+import 'ace-builds/src-noconflict/ext-language_tools';
+import 'ace-builds/src-noconflict/snippets/latex';
+import 'ace-builds/src-noconflict/mode-html';
+import 'ace-builds/src-noconflict/snippets/html';
+import 'ace-builds/src-noconflict/mode-latex';
+import 'ace-builds/src-noconflict/theme-dracula';
 
 @Component({
 	selector: 'app-editor',
 	templateUrl: './editor.component.html',
 	styleUrls: ['./editor.component.scss'],
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent implements AfterViewInit {
 
-	constructor() {
-	}
+	@ViewChild('editor') private editor!: ElementRef<HTMLElement>;
 
-	ngOnInit(): void {
+	ngAfterViewInit(): void {
+		ace.require('ace/ext/language_tools');
+
+		ace.config.set('fontSize', '14px');
+
+		ace.config.set('basePath', 'https://unpkg.com/ace-builds@1.4.12/src-noconflict');
+
+		const aceEditor = ace.edit(this.editor.nativeElement);
+		aceEditor.session.setValue('');
+		aceEditor.setOptions({
+			enableBasicAutocompletion: true,
+			enableSnippets: true,
+			enableLiveAutocompletion: true,
+		});
+
+		aceEditor.setTheme('ace/theme/dracula');
+		aceEditor.session.setMode('ace/mode/latex');
+
+		aceEditor.on('blur', () => {
+			console.log(aceEditor.getValue());
+		});
 	}
 
 }
