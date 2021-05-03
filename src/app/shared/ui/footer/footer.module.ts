167437e3b9bc35aee86466f2634089b1eaab119d
LopsemPyier <LopsemPyier@gmail.com>
2021-05-03 10:16:39 +0200

    REFACTOR v0.1.4 Fixing footer
    REFACTOR:
     - Redo footer

--- src/app/shared/ui/footer/footer.module.ts
+++ src/app/shared/ui/footer/footer.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
 import { IconModule } from '../icon/icon.module';
 import { RouterModule } from '@angular/router';
+import { LogoModule } from '../logo/logo.module';
 
 @NgModule({
 	declarations: [
@@ -12,6 +13,7 @@ import { RouterModule } from '@angular/router';
 		CommonModule,
 		IconModule,
 		RouterModule,
+		LogoModule,
 	],
 	exports: [
 		FooterComponent,
